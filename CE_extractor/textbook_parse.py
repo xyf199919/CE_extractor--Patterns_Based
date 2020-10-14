222032fb156cbd5257c8e9dba507b1d43c0b1717
Manav Rathod <manav.rathod@berkeley.edu>
2020-10-14 11:57:14 -0400

    updated textbook_parse.py

--- CE_extractor/textbook_parse.py
+++ CE_extractor/textbook_parse.py
@@ -11,33 +11,58 @@ from nltk.tree import *
 from nltk.corpus import wordnet as wn
 from tqdm import tqdm
 
-from cmyPackage import *
-from cmyPatternMatching import TXT2Patterns, OrderPatterns, MainTokenRegExp
+from cmyPatternMatching import TXT2Patterns, OrderPatterns, MainTokenRegExp, GettingCEcases
+
+# --------- people hypernym set ------------------
+PEOPLE = ('person', 'individual', 'someone', 'somebody', 'mortal', 'soul', 'being', 'human', 'hominid', 'humans', 'man',
+          'mankind')
+### ---- stanford_parser java package ----  
+os.environ['STANFORD_PARSER'] = r'../../stanford-parser.jar'
+os.environ['STANFORD_MODELS'] = r'../../stanford-parser-3.5.2-models.jar'
+### ---- JAVA_HOME path ----
+java_path = r"C:\Program Files\Java\jdk1.8.0_45\bin\java.exe"
+os.environ['JAVAHOME'] = java_path
+### ---- initiate a parser ---- 
+parser = stanford.StanfordParser(model_path=r"../../englishPCFG.ser.gz")
+
+def ptree(sent):
+    return parser.raw_parse(sent)
 
 nlp = spacy.load('en_core_web_sm')
 
 Patterns = TXT2Patterns()
 Patterns = OrderPatterns(Patterns, True)
 mtRegExplst = MainTokenRegExp(Patterns)
+emptyTree = Tree('ROOT', [])
 
 with codecs.open('textbook.csv', 'w', 'utf8') as csvfile:
     writer = csv.writer(csvfile, delimiter=',',
                                 quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(["Pattern", "Text", "Cause", "Connector", "Effect"])
+    writer.writerow(["Pattern", "Text", "Cause", "Effect"])
     with open("tqa/tqa_train_val_test/train/tqa_v1_train.json", "r") as f:
         data = json.loads(f.read())
         for d in tqdm(data):
             topics = d['topics']
             for t in topics:
                 text = d['topics'][t]['content']['text']
-                sents = [i.text for i in nlp(text).sents]
-                for s in sents:
-                    if "Figure" in s:
+                textbook_sentences = [i.text for i in nlp(text).sents]
+                tlen = len(textbook_sentences)
+                for ti in range(len(textbook_sentences)):
+                    sent = textbook_sentences[ti]
+                    pppt = ptree(sent)
+                    if pppt == []:
                         continue
-                    for p in mtRegExplst:
-                        if p.pattern == '(.*)':
-                            continue
-                        found = p.findall(s)
-                        if found:
-                            writer.writerows([[p.pattern, s] + ([f] if f and isinstance(f, unicode) else list(f)) for f in found])
-                            break
+                    curPT = pppt[0]
+                    prePT = emptyTree if ti < 1 or ptree(textbook_sentences[ti - 1]) == [] else ptree(textbook_sentences[ti - 1])[0]
+                    nextPT = emptyTree if ti > tlen - 2 or ptree(textbook_sentences[ti + 1]) == [] else ptree(textbook_sentences[ti + 1])[0]
+                    sentCEset = GettingCEcases(Patterns, mtRegExplst, prePT, curPT, nextPT)
+                    if sentCEset == []:
+                        continue
+                    for ce in sentCEset:
+                        writer.writerow([
+                            ' %s' % ce.pt.main_token + "----" + ' %s' % ce.pt.constraints,
+                            sent,
+                            ' '.join(ce.cause.PTree.leaves()),
+                            ' '.join(ce.effect.PTree.leaves())
+                        ])
+                    
